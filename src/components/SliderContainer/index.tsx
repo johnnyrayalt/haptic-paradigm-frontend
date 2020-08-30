f4e185cb32a6e3309ebcd75a7138485d4eac27a0
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-08-29 22:02:56 -0700

    SliderContainer: refactor how slider data is handled and how slider components are stood up

--- src/components/SliderContainer/index.tsx
+++ src/components/SliderContainer/index.tsx
@@ -1,34 +1,56 @@
 import React from 'react';
 import Slider from 'components/Slider';
 import { useSelector } from 'react-redux';
-import { CONSTANTS } from 'resources/constants';
+import { SLIDER, SLIDER_DATA, SLIDER_NAME } from 'resources/constants';
 import { actions } from 'store/actions';
 import Wavy from 'components/Wavy';
 import './sliderContainerStyles.scss';
+import { OscMessage } from 'interfaces/IOscMessage';
 
 const SliderContainer = () => {
-	const freqValue: number = useSelector((state: any) => state.frequencySlider.value);
-	const ampValue: number = useSelector((state: any) => state.amplitudeSlider.value);
+	const sliderValues: { [name: string]: any } = {
+		x: useSelector((state: any) => state.sliderX.value),
+		y: useSelector((state: any) => state.sliderY.value),
+		z: useSelector((state: any) => state.sliderZ.value),
+	};
 
-	return (
-		<div className='slider-container'>
-			<Wavy freq={freqValue === 0 ? 1 : freqValue} />
-			<Slider
-				name={CONSTANTS.FREQUENCY_SLIDER}
-				value={freqValue}
-				type={'f'}
-				actionType={actions.UPDATE_VALUE_FREQUENCY}
-			/>
+	const assembleSliderOpts = (opts: { address: string; value: number }[]): OscMessage[] => {
+		const sliderOpts: OscMessage[] = [];
+		opts.forEach((opt) => sliderOpts.push(SLIDER(opt.address, opt.value)));
 
-			<Wavy amp={ampValue === 0 ? 1 : ampValue} />
-			<Slider
-				name={CONSTANTS.AMPLITUDE_SLIDER}
-				value={ampValue}
-				type={'a'}
-				actionType={actions.UPDATE_VALUE_AMPLITUDE}
-			/>
-		</div>
-	);
+		return sliderOpts;
+	};
+
+	const returnSliders = (): JSX.Element[] => {
+		const sliderData = assembleSliderOpts(SLIDER_DATA);
+
+		return sliderData.map((sliderOpts: OscMessage) => {
+			const name = sliderOpts.address.split(`/${SLIDER_NAME}/`).pop();
+
+			return (
+				<div key={name}>
+					<Wavy
+						axis={sliderOpts.args[0].type}
+						value={
+							sliderValues[sliderOpts.args[0].type] === 0
+								? 1
+								: sliderValues[sliderOpts.args[0].type]
+						}
+					/>
+					<Slider
+						key={name}
+						opts={sliderOpts}
+						value={sliderValues[sliderOpts.args[0].type]}
+						actionType={
+							actions[`UPDATE_VALUE_SLIDER_${sliderOpts.args[0].type.toUpperCase()}`]
+						}
+					/>
+				</div>
+			);
+		});
+	};
+
+	return <div className='slider-container'>{returnSliders()}</div>;
 };
 
 export default SliderContainer;
