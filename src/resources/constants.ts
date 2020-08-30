ec6a78560b2fa75d5d1f801ecbc48bcfcfb28300
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-08-29 22:04:53 -0700

    Constants: refactor how slider data is assembled for OSC and source of truth for slider init data

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -1,7 +1,33 @@
-export const CONSTANTS = {
-	SLIDER_MIN: 0,
-	SLIDER_STEP: 1,
-	SLIDER_MAX: 100,
-	FREQUENCY_SLIDER: '/slider/frequency',
-	AMPLITUDE_SLIDER: '/slider/amplitude',
-};
+import { OscMessage } from 'interfaces/IOscMessage';
+export const SLIDER_MIN = 0;
+export const SLIDER_STEP = 1;
+export const SLIDER_MAX = 100;
+
+export const SLIDER_NAME: string = 'cube';
+export const SLIDER = (address: string, value: number, name: string = SLIDER_NAME): OscMessage => ({
+	address: `/${name}/${address}`,
+	args: [
+		{
+			type: address,
+			value: value,
+		},
+	],
+});
+
+export const SLIDER_DATA = [
+	{
+		address: 'x',
+		value: 50,
+		displayName: 'horizontal',
+	},
+	{
+		address: 'y',
+		value: 50,
+		displayName: 'vertical',
+	},
+	{
+		address: 'z',
+		value: 50,
+		displayName: 'depth',
+	},
+];
