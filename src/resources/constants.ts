c611e044a8eb1f2b51b3267c65dcc049d804c22b
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-09-05 19:11:16 -0700

    constants: add video and featured artist

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -1,4 +1,4 @@
-import { OscMessage } from 'interfaces/IOscMessage';
+import { OscMessage } from 'interfaces/Types/IOscMessage';
 export const SLIDER_MIN = 0;
 export const SLIDER_STEP = 1;
 export const SLIDER_MAX = 100;
@@ -31,3 +31,14 @@ export const SLIDER_DATA = [
 		displayName: 'depth',
 	},
 ];
+
+export const LOCAL_VIDEO_PARENT: string = '&parent=localhost';
+export const HOSTED_VIDEO_PARENT: string =
+	'&parent=haptic-paradigm.com&parent=www.haptic-paradigm.com';
+
+export const TWITCH_CHANNEL_ID: string = '?channel=tealportals';
+export const TWITCH_VIDEO_ID: string = '?video=732720305';
+
+export const FEATURED_ARTIST_NAME: string = 'Brenna Murphy';
+export const FEATURED_ARTIST_BIO: string =
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
