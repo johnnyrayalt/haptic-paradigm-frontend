f11add52bff6518dc300336ef0668324b5211edf
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-09-05 19:15:09 -0700

    WelcomePage: replace spans with TimeStamp component and replace iframe with VideoPlayer component

--- src/views/WelcomePage.tsx
+++ src/views/WelcomePage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './textPageStyles.scss';
-import moment from 'moment';
+import VideoPlayer from 'components/VideoPlayer';
+import TimeStamp from 'components/TimeStamp';
 
 const textPage = () => {
 	return (
@@ -9,65 +10,46 @@ const textPage = () => {
 			<h1 className='text-header'>welcome</h1>
 			<div className='text-text-container'>
 				<p className='text-text important-text'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					The gallery is currently closed but here is audio documentation of our last
 					interaction.
 				</p>
-				<iframe
-					title='player'
-					src='https://vimeo.com/event/140098/embed'
-					frameBorder='0'
-					allow='fullscreen'
-					allowFullScreen={false}
-					className='embedded-player'
-				></iframe>
+				<VideoPlayer videoType={'past'} />
 				<div>
 					<Link className='text-link' to='/about'>
 						Learn more about this project
 					</Link>
 				</div>
 				<p className='text-text'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					Haptic Paradigm is an interactive installation conceptualized by Chloe Alexandra
 					Thompson with web tools by Johnny Ray Alt. The project explores the ways we
 					remain in relationship to one another, despite being in isolation and outside of
 					immediate view.
 				</p>
 				<p className='text-text'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					When you arrive in the space, you’ll find two sliders that you can use to
 					manipulate the composition. You will see them moving but will not be able to
 					interact until it’s your turn. Sliders are marked but what you are controlling
 					will shift over time.
 				</p>
 				<p className='text-text'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					Guests may take short turns interacting with the piece for a 5 hour window. When
 					you arrive in the space, you’ll find two sliders that you can use to manipulate
 					the composition. You will see them moving but will not be able to interact until
 					it’s your turn.
 				</p>
 				<p className='text-text'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					To hear the results of the interactions, hit play on the embedded livestream on
 					the webpage. If someone is ahead of you in the queue, you’re encouraged to wait
 					and listen. Once the controls are yours, you’ll be able to manipulate the sonic
 					and visual fields.
 				</p>
 				<Link className='text-link off' to='/control-room'>
-					<span>
-						{moment().format('L HH:mm')} :\{'>'}
-					</span>{' '}
+					<TimeStamp />
 					Take me to the control room_
 				</Link>
 			</div>
