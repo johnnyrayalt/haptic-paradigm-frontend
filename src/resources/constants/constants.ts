5eaa22bb29fa330698997b7e184e1386f8542853
Johnny Ray Alt <johnnyrayalt@gmail.com>
2020-10-08 00:28:51 -0700

    constants: updates

--- src/resources/constants/constants.ts
+++ src/resources/constants/constants.ts
@@ -1,27 +1,7 @@
-import {
-	EXHIBITION_DATE,
-	FEATURED_ARTIST_NAME,
-	ADDITIONAL_EXHIBITION_INFO,
-} from './currentProjectConstants';
-import { OscMessage } from 'interfaces/Types/TOscMessage';
-import moment from 'moment-timezone';
-
-export const SLIDER_MIN: number = 0;
-export const SLIDER_STEP: number = 1;
-export const SLIDER_MAX: number = 100;
+import { EXHIBITION_DATE, FEATURED_ARTIST_NAME } from './currentProjectConstants';
+// import moment from 'moment-timezone';
 
 export const BLUE: string = '#2727e6';
-
-export const SLIDER_NAME: string = 'cube';
-export const SLIDER = (address: string, value: number, name: string = SLIDER_NAME): OscMessage => ({
-	address: `/${name}/${address}`,
-	args: [
-		{
-			type: address,
-			value: value,
-		},
-	],
-});
 
 export const LOCAL_VIDEO_PARENT: string = '&parent=localhost';
 export const HOSTED_VIDEO_PARENT: string =
@@ -30,10 +10,10 @@ export const HOSTED_VIDEO_PARENT: string =
 export const TWITCH_CHANNEL_ID: string = '?channel=tealportals';
 export const TWITCH_VIDEO_ID: string = '?video=732720305';
 
-const setDate = (hours: number) => {
-	const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-	return moment(`2020-09-13T${hours}:00:00Z`).tz(timezone).format('LT');
-};
+// const setDate = (hours: number) => {
+// 	const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// 	return moment(`2020-09-13T${hours}:00:00Z`).tz(timezone).format('LT');
+// };
 
 export const EXHIBITION_INFO = (): string =>
 	`The next public viewing will be ${EXHIBITION_DATE} featuring visuals from ${FEATURED_ARTIST_NAME}`;
--- src/resources/constants/uiConstants.ts
+++ src/resources/constants/uiConstants.ts
@@ -1,4 +1,5 @@
 import { TSliderData } from 'interfaces/Types/TSliderData';
+import { OscMessage } from 'interfaces/Types/TOscMessage';
 
 const UI_SCHEMES: { [name: string]: string } = {
 	XY_PAD: 'xypad',
@@ -6,6 +7,35 @@ const UI_SCHEMES: { [name: string]: string } = {
 };
 export const CURRENT_UI_SCHEME: string = UI_SCHEMES.XY_PAD;
 
+export const MESSAGE = (address: string, value: number, name: string): OscMessage => ({
+	address: `/${name}/${address}`,
+	args: [
+		{
+			type: address,
+			value: value,
+		},
+	],
+});
+
+// XYPAD DATA
+export const XYPAD_NAME: string = 'xypad';
+export const XYPAD_DATA: {}[] = [
+	{
+		address: 'x',
+		value: 50,
+		displayName: 'X',
+	},
+	{
+		address: 'y',
+		value: 50,
+		displayName: 'Y',
+	},
+];
+
+// SLIDER DATA
+export const SLIDER_MIN: number = 0;
+export const SLIDER_STEP: number = 1;
+export const SLIDER_MAX: number = 100;
 export const SLIDER_NAME: string = 'cube';
 export const SLIDER_DATA: TSliderData[] = [
 	{
